0762cd35c7edceef803a658917b5342801101acb
ayslanmarcelino <ayslan_marcelino@outlook.com>
2020-05-03 19:14:14 -0300

    add all frontend of rest api

---
+++ js/action.js
@@ -0,0 +1,147 @@
+﻿read();
+function read() {
+  const line = document.getElementById('line');
+
+  const http = new XMLHttpRequest();
+  http.open('GET', 'http://localhost:3000/people/find/', true);
+
+  http.onreadystatechange = function() {
+    if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      const objJSON = JSON.parse(http.responseText);
+
+      var pageLength = 9;
+      var numberPage = 0;
+
+      function page() {
+        $('table > tbody > tr').remove();
+        var tbody = $('table > tbody');
+        for (var data = numberPage * pageLength; data < objJSON.length && data < (numberPage + 1) *  pageLength; data++) {
+          tbody.append(
+              $('<tr>')
+                .append($('<td>').append(objJSON[data].code))
+                .append($('<td>').append(objJSON[data].name))
+                .append($('<td>').append(objJSON[data].age))
+                .append($('<td>').append(objJSON[data].email))
+                .append($('<td>').append(`<button class="btn btn-info" onclick="select(${objJSON[data].code})">
+                                            Selecionar
+                                          </button>`))
+          )
+        }
+        $('#numeration').text('Página ' + (numberPage + 1) + ' de ' + Math.ceil(objJSON.length / pageLength));
+      }
+
+      function adjustButtons() {
+        $('#next').prop('disabled', objJSON.length <= pageLength || numberPage > objJSON.length / pageLength - 1);
+        $('#previous').prop('disabled', objJSON.length <= pageLength || numberPage == 0);
+      }
+
+      $(function() {
+        $('#next').click(function() {
+          if (numberPage < objJSON.length / pageLength - 1) {
+              numberPage++;
+              page();
+              adjustButtons();
+          }
+        });
+        $('#previous').click(function() {
+          if (numberPage > 0) {
+              numberPage--;
+              page();
+              adjustButtons();
+          }
+        });
+        page();
+        adjustButtons();
+      })
+
+      let countRecord = `Total de Registros: ${objJSON.length}`
+      count.innerHTML = countRecord;
+    }
+  }
+  http.send();
+}
+
+function select(_code=0) {
+  const code = document.getElementById('code');
+  const name = document.getElementById('name');
+  const age = document.getElementById('age');
+  const email = document.getElementById('email');
+
+  const http = new XMLHttpRequest();
+  http.open('GET', 'http://localhost:3000/people/find/'+_code, true);
+
+  http.onreadystatechange = function() {
+    if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      const objJSON = JSON.parse(http.responseText);
+
+      code.value = objJSON.code;
+      name.value = objJSON.name;
+      age.value = objJSON.age;
+      email.value = objJSON.email;
+    }
+  }
+  http.send();	
+}
+
+function clean() {
+  document.getElementById('code').value = 0;
+  document.getElementById('name').value = '';
+  document.getElementById('age').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('name').focus();
+}
+
+function save() {
+  const code = Number(document.getElementById('code').value);
+  const name = document.getElementById('name').value.toString().trim();
+  const age = Number(document.getElementById('age').value);
+  const email = document.getElementById('email').value.toString().trim();
+
+  let url = '';
+  let method = '';
+  let params = {};
+  let strJSON = '';
+  if(code>0) {
+    method = 'PUT';
+    url = 'http://localhost:3000/people/edit/'+code;
+    if(name.length>0) params.name = name;
+    if(age>0) params.age = age;
+    if(email.length>0) params.email = email;
+  }else {
+    method = 'POST';
+    url = 'http://localhost:3000/people/create';
+    params.name = name;
+    params.age = age;
+    params.email = email;
+  }
+  strJSON = JSON.stringify(params);
+
+  const http = new XMLHttpRequest();
+  http.open(method, url, true);
+  http.setRequestHeader('Content-Type', 'application/json');
+
+  http.onreadystatechange = function() {
+    if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      read();
+      clean();
+    }
+  }
+  http.send(strJSON);	
+}
+
+function deletePerson() {
+  const code = Number(document.getElementById('code').value);
+
+  if(code>0) {
+    const http = new XMLHttpRequest();
+    http.open('DELETE', 'http://localhost:3000/people/delete/'+code, true);
+
+    http.onreadystatechange = function() {
+      if(this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        read();
+        clean();
+      }
+    }
+    http.send();
+  }	
+}
